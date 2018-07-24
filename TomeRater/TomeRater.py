ede76056e3d5dea936952ab6f4c5ce39d658f68c
phzang <40149803+phzang@users.noreply.github.com>
2018-07-24 12:30:30 -0400

    initial commit

---
+++ TomeRater/TomeRater.py
@@ -0,0 +1,37 @@
+class User(object):
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.books = {}
+
+    def get_email(self):
+        return self.email
+
+    def change_email(self, address):
+        self.email = address
+        print("User {name} email has been updated to {email}".format( \
+            name = self.name, email = self.email))
+
+    def __repr__(self):
+        pass
+
+    def __eq__(self, other_user):
+        pass
+
+class Book(object):
+    def __init__(self, title, isbn):
+        self.title = title
+        self.isbn = isbn
+        self.ratings = []
+
+    def get_title(self):
+        return self.title
+
+    def get_isbn(self):
+        return self.isbn
+
+    def set_isbn(new_isbn):
+        self.isbn = new_isbn
+        print("{title} ISBN has been updated to {isbn}.".format( \
+            title = self.title
+            isbn = self.isbn
