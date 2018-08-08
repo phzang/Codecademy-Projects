be5770e0342799369877838bfae011f9f16b851f
phzang <40149803+phzang@users.noreply.github.com>
2018-08-08 13:34:30 -0400

    updated various to PEP8

--- TomeRater/TomeRater.py
+++ TomeRater/TomeRater.py
@@ -2,15 +2,15 @@ class User(object):
     def __init__(self, name, email):
         self.name = name
         self.email = email
-        self.books = {} #Book(object) : rating
+        self.books = {}  # Book(object) : rating
 
     def get_email(self):
         return self.email
 
     def change_email(self, address):
         self.email = address
-        print("User {name}'s email has been updated to {email}".format( \
-            name = self.name, email = self.email))
+        print("User {name}'s email has been updated to {email}".format(
+            name=self.name, email=self.email))
 
     def read_book(self, book, rating=None):
         self.books[book] = rating
@@ -19,7 +19,7 @@ class User(object):
         total_rating = 0
         books_with_ratings = 0
         for rating in self.books.values():
-            if rating != None:
+            if rating is not None:
                 total_rating += rating
                 books_with_ratings += 1
         if books_with_ratings:
@@ -30,14 +30,15 @@ class User(object):
         return len(self.books)
 
     def __repr__(self):
-        return "User {user}, email: {email}, books read: {books_read}".format( \
-            user = self.name, email = self.email, books_read = len(self.books))
+        return "User {user}, email: {email}, books read: {books_read}".format(
+            user=self.name, email=self.email, books_read=len(self.books))
 
     def __eq__(self, other_user):
         return (self.name == other_user.name) and (self.email == other_user.email)
 
     def __hash__(self):
         return hash((self.name, self.email))
+
 
 class Book(object):
     def __init__(self, title, isbn, price = None):
@@ -62,7 +63,7 @@ class Book(object):
         total_rating = 0
         valid_ratings = 0
         for r in self.ratings: 
-            if r != None:
+            if r is not None:
                 total_rating += r
                 valid_ratings += 1
         if valid_ratings:
@@ -71,22 +72,22 @@ class Book(object):
 
     def set_isbn(self, new_isbn):
         self.isbn = new_isbn
-        print("{title} ISBN has been updated to {isbn}.".format( \
-            title = self.title,
-            isbn = self.isbn))
+        print("{title} ISBN has been updated to {isbn}.".format(
+            title=self.title,
+            isbn=self.isbn))
 
     def set_price(self, new_price):
         if new_price >= 0:
             self.price = new_price
-            print("{title} price has been updated to {price}.".format( \
-                title = self.title,
-                price = self.price))
+            print("{title} price has been updated to {price}.".format(
+                title=self.title,
+                price=self.price))
         else:
             print("Please enter a valid price.")
 
     def add_rating(self, rating):
-        #rating == None prevents TypeError >= 'NoneType' and 'int'
-        if  (rating is None) or (rating >= 0 and rating <= 4):
+        # rating == None prevents TypeError >= 'NoneType' and 'int'
+        if (rating is None) or (rating >= 0 and rating <= 4):
             self.ratings.append(rating)
         else:
             print("Invalid Rating")
@@ -95,8 +96,9 @@ class Book(object):
         return (self.title == other_user.title) and (self.isbn == other_user.isbn)
 
     def __repr__(self):
-        return "{title}".format( \
-            title = self.title)
+        return "{title}".format(
+            title=self.title)
+
 
 class Fiction(Book):
     def __init__(self, title, author, isbn):
@@ -107,15 +109,16 @@ class Fiction(Book):
         return self.author
 
     def __repr__(self):
-        return "{title} by {author}".format( \
-            title = self.title,
-            author = self.author)
+        return "{title} by {author}".format(
+            title=self.title,
+            author=self.author)
+
 
 class Non_Fiction(Book):
     def __init__(self, title, subject, level, isbn):
         super().__init__(title, isbn)
-        self.subject = subject #string
-        self.level = level #string
+        self.subject = subject  # string
+        self.level = level  # string
 
     def get_subject(self):
         return self.subject
@@ -124,14 +127,15 @@ class Non_Fiction(Book):
         return self.level
 
     def __repr__(self):
-        return "{title}, a {level} manual on {subject}".format( \
-            title = self.title, level = self.level, subject = self.subject)
+        return "{title}, a {level} manual on {subject}".format(
+            title=self.title, level=self.level, subject=self.subject)
+
 
 class TomeRater(object):
     def __init__(self):
-        self.users = {} #email : User(object)
-        self.books = {} #Book(object) : number of Users that have read it
-        self.library = {} #ISBN : Book(objects) that were created
+        self.users = {}  # email : User(object)
+        self.books = {}  # Book(object) : number of Users that have read it
+        self.library = {}  # ISBN : Book(objects) that were created
 
     def isbn_exists(self, isbn):
         if isbn in self.library:
@@ -173,13 +177,13 @@ class TomeRater(object):
     def add_user(self, name, email, user_books=None):
         if self.valid_email(email):
             if email in self.users:
-                print("User with email {email} already exists!".format( \
-                                                    email = email))
+                print("User with email {email} already exists!".format(
+                                                    email=email))
             else:
-                self.users[email] = User(name,email)
+                self.users[email] = User(name, email)
                 if user_books:
                     for b in user_books:
-                        self.add_book_to_user(b,email)
+                        self.add_book_to_user(b, email)
             return True
         else:
             print("Please enter a valid email")
@@ -194,18 +198,18 @@ class TomeRater(object):
             print(user)
 
     def get_most_read_book(self):
-        return (max(self.books,key=self.books.get))
+        return max(self.books, key=self.books.get)
 
     def get_worth_of_user(self, user_email):
         total = 0
         if user_email in self.users:
             for book in self.users[user_email].books:
-                if book.get_price() != None:
-                   total += book.get_price()
+                if book.get_price() is not None:
+                    total += book.get_price()
         return total
 
     def highest_rated_book(self):
-        tmp_book = Book("","")
+        tmp_book = Book("", "")
         high_rating = 0
         for book in self.books:
             if book.get_average_rating() >= high_rating:
@@ -214,7 +218,7 @@ class TomeRater(object):
         return tmp_book
 
     def most_positive_user(self):
-        tmp_user = User("","")
+        tmp_user = User("", "")
         high_rating = 0
         for user in self.users.values():
             if user.get_average_rating() >= high_rating:
@@ -229,8 +233,8 @@ class TomeRater(object):
                 email_dot_split = str(email_split[1]).split('.')
             
                 if (email_dot_split[1] == "com") or  \
-                    (email_dot_split[1] == "edu") or  \
-                    (email_dot_split[1]) == "org":
+                   (email_dot_split[1] == "edu") or  \
+                   (email_dot_split[1]) == "org":
                     return True
         
         except IndexError:
@@ -239,11 +243,11 @@ class TomeRater(object):
 
     def get_n_most_read_books(self, n):
         sorted_books = sorted(self.books, key=self.books.get, reverse=True)
-        #prevent Index out of range error
+        # prevent Index out of range error
         if n > len(sorted_books):
             n = len(sorted_books)
         tmp = []
-        for b in range(0,n):
+        for b in range(0, n):
             tmp.append(sorted_books[b])
         return tmp
 
@@ -253,24 +257,24 @@ class TomeRater(object):
             tester[book] = book.get_price()
             
         sorted_readers = sorted(tester, key=tester.get, reverse=True)
-        #prevent Index out of range error
+        # prevent Index out of range error
         if n > len(sorted_readers):
             n = len(sorted_readers)
 
         tmp = []
-        for b in range(0,n):
+        for b in range(0, n):
             tmp.append(sorted_readers[b])
         return tmp
         
     def __repr__(self):
         total = 0
         for book in self.books:
-            if book.get_price() != None:            
+            if book.get_price() is not None:
                 total += book.get_price()
-        return "Tome Rater has {users} users, {books} books, worth ${total}".format( \
-                users = len(self.users), books = len(self.books), total = total)
+        return "Tome Rater has {users} users, {books} books, worth ${total}".format(
+                users=len(self.users), books=len(self.books), total=total)
                  
     def __eq__(self, other):
-        return ((self.users == other.users) and \
-                (self.books == other.books) and \
+        return ((self.users == other.users) and
+                (self.books == other.books) and
                 (self.library == other.library))    
